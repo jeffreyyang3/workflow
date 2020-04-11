097ae6bca98b0424888821ff2ddd7e6c4c6a6469
Jeffrey Yang <jeffreyhasmail@gmail.com>
2020-04-11 11:56:12 -0700

    reorganize

---
+++ build/precache-manifest.64e843c8b3b4484d8196e20464aba425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cbd096d7eb4992a732723de56c0a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919384fa1ace9fad332b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90047e304c79690baa7d",
+    "url": "/static/js/2.97ac4a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97ac4a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919384fa1ace9fad332b",
+    "url": "/static/js/main.28402e55.chunk.js"
+  },
+  {
+    "revision": "d0b755c100e2c0568822",
+    "url": "/static/js/runtime-main.331c8980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
